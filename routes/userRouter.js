c0740cb735bbfd189861c82990d836d2796a5129
PolinaVasilevich <polina.vasilevichh@gmail.com>
2024-03-22 20:09:24 +0100

    create userRouter and userController

---
+++ routes/userRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const { login, register, current } = require("../controllers/userController");
+
+router.post("/login", login);
+
+router.post("/register", register);
+
+router.get("/current", current);
+
+module.exports = router;
