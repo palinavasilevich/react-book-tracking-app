36e27fef50952a5c2e4cea0fc1afe0b0e23ed15a
PolinaVasilevich <polina.vasilevichh@gmail.com>
2024-03-23 14:24:01 +0100

    created auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { prisma } = require("../prisma/prisma-client");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      res.status(401).json({ message: "No authorization" });
+    }
+
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await prisma.user.findUnique({ where: { id: decoded.id } });
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: "No authorization" });
+  }
+};
+
+module.exports = { auth };
